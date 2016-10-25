3adad5ff9728f83d242b1b57462d8bc76e8ad4b2
Wuilliam Lacruz <wuilliam321@gmail.com>
2016-10-25 12:07:50 -0400

    Agregado el passport plugin, ahora autentica correctamente, incorporado la lista de planillas con vue y usando el api

---
+++ resources/views/planillas/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <planillas-index></planillas-index>
+        </div>
+    </div>
+</div>
+@endsection
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,12 @@ use Illuminate\Http\Request;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
+
+Route::get('/planillas', function (App\Planilla $planilla) {
+    return DB::table('planillas')
+        ->join('colaboradores', 'colaboradores.id', '=', 'planillas.colaborador_id')
+        ->join('proyectos', 'proyectos.id', '=', 'planillas.proyecto_id')
+        ->select('planillas.*', 'colaboradores.nombre as nombre_colaborador', 'colaboradores.cedula',  'colaboradores.tipo_salario', 'proyectos.nombre as nombre_proyecto')
+        ->get();
+
+})->middleware('auth:api');
