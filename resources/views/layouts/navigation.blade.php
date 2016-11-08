2d50ab2e3a71944c5067c79c5de7494daf5bc595
Wuilliam Lacruz <wuilliam321@gmail.com>
2016-11-08 13:40:21 -0500

    Remove logger link

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -2,12 +2,14 @@
     <div class="sidebar-collapse">
         <div class="logo"></div>
         <ul class="nav metismenu" id="side-menu">
-            <li class="{{ isActiveRoute('main') }}">
-                <a href="{{ url('/') }}"><i class="fa fa-th-large"></i> <span class="nav-label">Planillas</span> </a>
-            </li>
-            <li class="{{ isActiveRoute('logger') }}">
-                <a href="{{ url('/logger') }}" target="_blank"><i class="fa fa-th-large"></i> <span class="nav-label">Log de cambios</span> </a>
-            </li>
+            @if (Auth::guest())
+                <li><a href="{{ url('/login') }}"><i class="fa fa-unlock"></i> <span class="nav-label">Login</span></a></li>
+                <li><a href="{{ url('/register') }}"><i class="fa fa-user-plus"></i> <span class="nav-label">Register</span></a></li>
+            @else
+                <li class="{{ isActiveRoute('main') }}">
+                    <a href="{{ url('/') }}"><i class="fa fa-files-o"></i> <span class="nav-label">Planillas</span> </a>
+                </li>
+            @endif
         </ul>
 
     </div>
