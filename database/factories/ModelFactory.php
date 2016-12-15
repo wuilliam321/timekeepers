431cec2d50bf315d9b081900fa218a773685203b
Wuilliam Lacruz <wuilliam321@gmail.com>
2016-12-15 14:35:01 -0400

    User seeder now works

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -10,6 +10,16 @@
 | database. Just tell the factory how a default model should look.
 |
 */
+$factory->define(App\User::class, function (Faker\Generator $faker) {
+    static $password;
+
+    return [
+        'name' => $faker->name,
+        'email' => $faker->unique()->safeEmail,
+        'password' => $password ?: $password = bcrypt('secret'),
+        'remember_token' => str_random(10),
+    ];
+});
 
 $factory->define(App\CuentasBeneficio::class, function (Faker\Generator $faker) {
     return [
