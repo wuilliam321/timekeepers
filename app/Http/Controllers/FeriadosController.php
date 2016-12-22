800de9f0f592e5718beed2a4fb43bbef40c87db4
Wuilliam Lacruz <wuilliam321@gmail.com>
2016-12-22 18:31:54 -0400

    Little improvements in feriados controller

--- app/Http/Controllers/FeriadosController.php
+++ app/Http/Controllers/FeriadosController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Feriado;
 use Illuminate\Http\Request;
-
 use App\Http\Requests;
 use Illuminate\Support\Facades\Auth;
 
--- routes/api.php
+++ routes/api.php
@@ -50,9 +50,7 @@ Route::get('/cuentas_beneficios', 'CuentasBeneficiosController@getAllCuentas')
 Route::get('/procesar_horas', 'RecargosController@run');
 
 /** Feriados **/
-Route::get('/feriados', 'FeriadosController@getFeriados');
-Route::post('/feriados', 'FeriadosController@createFeriado');
-Route::put('/feriados/{id}', 'FeriadosController@updateFeriado');
-Route::delete('/feriados/{id}', 'FeriadosController@deleteFeriado');
-/*
-    ->middleware('auth:api')*/
+Route::get('/feriados', 'FeriadosController@getFeriados')->middleware('auth:api');
+Route::post('/feriados', 'FeriadosController@createFeriado')->middleware('auth:api');
+Route::put('/feriados/{id}', 'FeriadosController@updateFeriado')->middleware('auth:api');
+Route::delete('/feriados/{id}', 'FeriadosController@deleteFeriado')->middleware('auth:api');
